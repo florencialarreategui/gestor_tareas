bc1281dacd5208829de3961f3175252760ef2001
florencialarreategui <florencialarreategui@gmail.com>
2024-09-11 21:11:14 -0300

    modifico clase comentario

--- comentario.php
+++ comentario.php
@@ -1,29 +1,59 @@
 <?php
-class comentario{
+require_once 'usuario.php';
+require_once 'tarea.php';
+
+class Comentario {
     private $id_comentario;
-    private $descripcion_comentario;
-    private $estado_comentario;
+    private $id_tarea;
+    private $id_usuario;
+    private $contenido;
+    private $fecha;
 
+    public function __construct($id_comentario, $id_tarea, $id_usuario, $contenido, $fecha) {
+        $this->id_comentario = $id_comentario;
+        $this->id_tarea = $id_tarea;
+        $this->id_usuario = $id_usuario;
+        $this->contenido = $contenido;
+        $this->fecha = $fecha;
+    }
 
-public function __construct ($id_comentario, $descripcion_comentario, $estado_comentario){
-    $this-> id_comentario = $id_comentario;
-    $this-> descripcion_comentario = $descripcion_comentario;
-    $this-> estado_comentario = $estado_comentario;
-}
-public function getId_comentario (){
-    return $this-> Id_comentario;
-}
-public function getDescripcion_comentario (){
-    return $this-> descripcion_comentario;
-}
-public function getEstado_comentario(){
-    return $this-> estado_comentario;
-}
-public function setDescripcion_comentario ($descripcion_comentario){
-    $this-> descripcion_comentario = $descripcion_comentario;
-}
-public function setEstado_comentario ($estado_comentario){
-    $this-> estado_comentario= $estado_comentario;
-}
+    public function getIdComentario() {
+        return $this->id_comentario;
+    }
 
+    public function getIdTarea() {
+        return $this->id_tarea;
+    }
+
+    public function getIdUsuario() {
+        return $this->id_usuario;
+    }
+
+    public function getContenido() {
+        return $this->contenido;
+    }
+
+    public function getFecha() {
+        return $this->fecha;
+    }
+
+    public function setIdComentario($id_comentario) {
+        $this->id_comentario = $id_comentario;
+    }
+
+    public function setIdTarea($id_tarea) {
+        $this->id_tarea = $id_tarea;
+    }
+
+    public function setIdUsuario($id_usuario) {
+        $this->id_usuario = $id_usuario;
+    }
+
+    public function setContenido($contenido) {
+        $this->contenido = $contenido;
+    }
+
+    public function setFecha($fecha) {
+        $this->fecha = $fecha;
+    }
 }
