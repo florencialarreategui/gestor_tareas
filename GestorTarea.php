e43d729b140ceedf7a750566eb9b997b925e9657
florencialarreategui <florencialarreategui@gmail.com>
2024-10-28 12:48:24 -0300

    se arreglan funciones, crear, listar y editar tareas

--- GestorTarea.php
+++ GestorTarea.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'usuario.php';
-require_once 'proyecto.php';
+// require_once 'usuario.php';
+// require_once 'proyecto.php';
 require_once 'tarea.php';
-require_once 'comentario.php';
-require_once 'estado.php';
+// require_once 'comentario.php';
+// require_once 'estado.php';
 require_once 'tarea.json';
 
 class GestorTarea{
@@ -14,12 +14,34 @@ class GestorTarea{
         {
             $this->cargarDesdeJSON();
         }
+        //----------------- Agregar tarea--------------------------------
+        public function agregarTarea() {
 
-        public function agregarTarea($tarea) {
-            $this->tareas[] = $tarea;
+            $id_tarea = 1;
+                
+            echo "Ingrese el nombre de al tarea: ";
+            $nombre = trim(fgets(STDIN));
+
+            echo "Ingrese el descripción de la tarea: ";
+            $descripcion = trim(fgets(STDIN));
+
+            echo "Ingrese la fecha de inicio (YYYY-MM-DD): ";
+            $fecha_inicio= trim(fgets(STDIN));
+
+            echo "Ingrese la fecha de finalización (YYYY-MM-DD): ";
+            $fecha_fin = trim(fgets(STDIN));
+            $id_proyecto =1;
+            $id_usuario = 1;
+            $id_estado = 1;
+
+            $nuevaTarea = new Tarea($id_tarea, $nombre, $descripcion, $fecha_inicio, $fecha_fin, $id_proyecto, $id_usuario, $id_estado);
+            $this->tareas[] = $nuevaTarea;
+
+
+            echo "tarea creado exitosamente: " . $nuevaTarea->getNombre() ." ". $id_tarea . "\n";
             $this->guardarEnJSON();
         }
-
+//----------------- Obtener tarea--------------------------------
         public function obtenerTarea($id_tarea) {
             foreach ($this->tareas as $tarea) {
                 if ($tarea->getIdTarea() == $id_tarea) {
@@ -29,33 +51,76 @@ class GestorTarea{
             return null;
         }
 
-        public function actualizarTarea($id_tarea, $nombre, $descripcion, $fecha_inicio, $fecha_fin, $id_proyecto, $id_usuario, $id_estado) {
-            $tarea = $this->obtenerTarea($id_tarea);
-            if ($tarea) {
-                $tarea->setNombre($nombre);
-                $tarea->setDescripcion($descripcion);
-                $tarea->setFechaInicio($fecha_inicio);
-                $tarea->setFechaFin($fecha_fin);
-                $tarea->setIdProyecto($id_proyecto);
-                $tarea->setIdEstado($id_estado);
-                $tarea->setIdUsuario($id_usuario);
-                
+        //-------------listar Tareas--------------------------------------
+
+        public function listarTareas() {
+            if (empty($this->tareas)) {
+                echo "No hay tareas registradas.\n";
+                return;
             }
-            $this->guardarEnJSON();
+
+            echo "=== Tareas Registradas ===\n";
+            foreach ($this->tareas as $tarea) {
+                 echo "Id: " . $tarea->getIdTarea() . "  Nombre: " . $tarea->getNombre()  . "\n"; //" Descripción: ". $tarea->getDescripcion() "Fecha de Inicio: " . $tarea->getFechaInicio() . ", Fecha de Finalización: " . $tarea->getFechaFin() . "\n";
+            }
+
         }
 
-        public function eliminarTarea($id_tarea) {
-            foreach ($this->tareas as $index => $tarea) {
-                if ($tarea->getIdTarea() == $id_tarea) {
-                    unset($this->tareas[$index]);
-                    $this->tareas = array_values($this->tareas); // Reindexamos
+        //----------------- Editar tarea--------------------------------
+        // public function editarTarea($id_tarea, $nombre, $descripcion, $fecha_inicio, $fecha_fin, $id_proyecto, $id_usuario, $id_estado) {
+        //     $tarea = $this->obtenerTarea($id_tarea);
+        //     if ($tarea) {
+        //         $tarea->setNombre($nombre);
+        //         $tarea->setDescripcion($descripcion);
+        //         $tarea->setFechaInicio($fecha_inicio);
+        //         $tarea->setFechaFin($fecha_fin);
+        //         $tarea->setIdProyecto($id_proyecto);
+        //         $tarea->setIdEstado($id_estado);
+        //         $tarea->setIdUsuario($id_usuario);
                 
-                    break;
+        //     }
+        //     // $this->guardarEnJSON();
+        // }
+
+        public function editarTarea() {
+            echo "Ingrese el ID de la tarea que desea editar: ";
+            $id_tarea = trim(fgets(STDIN));
+            // Busca el proyecto por ID
+            foreach ($this->tareas as $tarea) {
+                if ($tarea->getIdTarea() == $id_tarea) {
+                    echo "Ingrese el nuevo nombre de la tarea: ";
+                    $nombre = trim(fgets(STDIN));
+                    $tarea->setNombre($nombre);
+                    echo "Ingrese la nueva descripción de la tarea: ";
+                    $descripcion = trim(fgets(STDIN));
+                    $tarea->setDescripcion($descripcion);
+                    echo "Ingrese la nueva fecha de inicio (YYYY-MM-DD): ";
+                    $fechaInicio = trim(fgets(STDIN));
+                    $tarea->setFechaInicio($fechaInicio);
+                    echo "Ingrese la nueva fecha de finalización (YYYY-MM-DD): ";
+                    $fechaFin = trim(fgets(STDIN));
+                    $tarea->setFechaFin($fechaFin);
+                    echo "Tarea editada exitosamente: " . $tarea->getNombre() . "\n";
+    
+                    $this->guardarEnJSON();
+                    return;
                 }
-               
             }
-            $this->guardarEnJSON();
+            echo "Tarea no encontrado.\n";
         }
+
+        // public function eliminarTarea($id_tarea) {
+        //     foreach ($this->tareas as $index => $tarea) {
+        //         if ($tarea->getIdTarea() == $id_tarea) {
+        //             unset($this->tareas[$index]);
+        //             $this->tareas = array_values($this->tareas); // Reindexamos
+                
+        //             break;
+        //         }
+               
+        //     }
+        //     // $this->guardarEnJSON();
+        // }
 
         public function guardarEnJSON() {
             $tareas = [];
@@ -95,3 +160,8 @@ class GestorTarea{
 
        
         }
+$nuevaTarea = new GestorTarea ();
+$nuevaTarea->agregarTarea();
+$nuevaTarea->listarTareas();
+$nuevaTarea->editarTarea();
+$nuevaTarea->listarTareas();
